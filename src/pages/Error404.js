87ff112d427ca79193b8f3b79482c1711e17d105
pfcardona <pfcardona@gmail.com>
2020-03-27 23:00:49 -0500

    Single Page Application Javascript Vanilla

---
+++ src/pages/Error404.js
@@ -0,0 +1,13 @@
+// creamos nuestro arrow fntcion que nos va a permitir convertir en una  función 
+// esta constante
+
+const Error404 = () => {
+    const view = `
+    <div class="Error404">
+    <h2>Error 404</h2>
+  </div>
+`;
+return view;
+};
+
+export default Error404;
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+// Permitir traer path. Acceder dentro de la carpte no importa el lugar donde se encuentre
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js'
+    },
+    resolve: {
+        extensions: ['.js'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                }
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin(
+            {
+                inject: true,
+                template: './public/index.html',
+                filename: './index.html'
+            }
+        )
+    ]
+}
